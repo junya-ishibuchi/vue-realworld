d34ddcd9e26626a5419294bf3a52c7c75dec3c71
Junya <junya.ishibuchi@gmail.com>
2024-01-17 11:16:32 -0800

    feat: implement header

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import Register from "./features/register/Register.vue";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
-createApp(App).mount('#app')
+const Home = { template: "<div>Home</div>" };
+const routes: RouteRecordRaw[] = [
+  { path: "/", component: Home },
+  { path: "/register", component: Register },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+createApp(App).use(router).mount("#app");
