c30939eda7f1d670b42f24c9c292a2199ad1c7c0
Junya <junya.ishibuchi@gmail.com>
2024-01-19 08:52:36 -0800

    fix: delete unnecessary css

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
 import Register from "./features/register/Register.vue";
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import NewArticle from "./features/articles/edit/NewArticle.vue";
 
 const Home = { template: "<div>Home</div>" };
 const routes: RouteRecordRaw[] = [
   { path: "/", component: Home },
   { path: "/register", component: Register },
+  { path: "/editor", component: NewArticle },
 ];
 
 const router = createRouter({
